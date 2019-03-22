cc1d72bc42078f50bc9b5d8f207fef0b6bc16069
Somat Thakali <sthakal000@citymail.cuny.edu>
2019-03-22 16:31:25 -0400

    Added printMovieInfo function in movie.js

--- javascript/movie.js
+++ javascript/movie.js
@@ -2,7 +2,8 @@ var keys = require("./keys.js");
 var userInput = require("./userInput");
 var axios = require("axios");
 
-/**
+module.exports = {
+  /**
  * This function will search the movie by name and will console log ---
  * Title of the movie.
  * Year the movie came out.
@@ -14,33 +15,39 @@ var axios = require("axios");
  * Actors in the movie.
 
  */
-module.exports = {
   movieSearch: function movieSearch() {
     var movieName = process.argv[3];
     var moviekey = keys.movieAPI;
-    var queryUrl =
-      "http://www.omdbapi.com/?t=" +
-      userInput.userInputString(movieName) +
-      "&y=&plot=short&apikey=" +
-      moviekey;
-    axios.get(queryUrl).then(response => {
-      // console.log(response.data);
-      console.log(
-        "Title: " +
-          response.data.Title.red.bold +
-          "\nYear: " +
-          response.data.Year.red.bold +
-          "\nimdb Rating: " +
-          response.data.imdbRating.red.bold +
-          "\nCountry produced : " +
-          response.data.Country.red.bold +
-          "\nLanguage: " +
-          response.data.Language.red.bold +
-          "\nPlot: " +
-          response.data.Plot.green.bold +
-          "\nActors: " +
-          response.data.Actors.red.bold
-      );
-    });
+    var queryUrl = "";
+    if (!movieName) {
+      queryUrl =
+        "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=" + moviekey;
+      axios.get(queryUrl).then(response => {
+        printMovieInfo(response);
+      });
+    } else {
+      queryUrl =
+        "http://www.omdbapi.com/?t=" +
+        userInput.userInputString(movieName) +
+        "&y=&plot=short&apikey=" +
+        moviekey;
+      axios.get(queryUrl).then(response => {
+        printMovieInfo(response);
+      });
+    }
   }
 };
+
+function printMovieInfo(response) {
+  console.log(
+    `\nTitle: ${response.data.Title.red.bold}\nYear: ${
+      response.data.Year.red.bold
+    }\nimdbRating: ${response.data.imdbRating.red.bold}\nCountry Produced: ${
+      response.data.Country.red.bold
+    }\nLanguage: ${response.data.Language.red.bold}\nPlot: ${
+      response.data.Plot.green.bold
+    }\nActors: ${
+      response.data.Actors.red.bold
+    }\n---------------------------------------------------`
+  );
+}
--- javascript/spotify.js
+++ javascript/spotify.js
@@ -2,15 +2,15 @@ var keys = require("./keys.js");
 var userInput = require("./userInput");
 var Spotify = require("node-spotify-api");
 
-/**
- * This function will search the follwing information by song's name ---
- * Artist(s)
- * The song's name
- * A preview link of the song from Spotify
- * The album that the song is from
- * */
 var spotify = new Spotify(keys.spotify);
 module.exports = {
+  /**
+   * This function will search the follwing information by song's name ---
+   * Artist(s)
+   * The song's name
+   * A preview link of the song from Spotify
+   * The album that the song is from
+   * */
   songSearch: function songSearch() {
     var songName = process.argv[3];
 
@@ -21,17 +21,17 @@ module.exports = {
         limit: 5
       })
       .then(function(response) {
-        //   console.log(response.tracks.items);
         var tracks = response.tracks.items;
         tracks.forEach(track => {
           var artists = track.album.artists;
           artists.forEach(artist => {
             console.log(
-              `Artist: ${artist.name.bold.red}${"\n"}Track Name: ${
-                track.name.bold.red
-              }`
+              `\nAlbum: ${track.album.name.bold.red}\nArtist: ${
+                artist.name.bold.red
+              }\nSong's Name: ${track.name.bold.red}\nPreview URL: ${
+                track.preview_url
+              }\n-----------------------------------------------`
             );
-            // console.log(track.name);
           });
         });
       })
